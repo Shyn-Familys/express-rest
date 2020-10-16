a10fe66385d068b941bb2cd9e3d6304ed358139c
tranthaison1231 <thaison.coderstokyo@gmail.com>
2020-10-16 10:36:33 +0700

    Update package

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as logger } from './providers/logger';
 export { default as Express } from './providers/Express';
 export { default as Response } from './providers/Response';
 export { default as Controller } from './providers/interface';
-export { default as Exception } from './providers/Exception';
+export { default as Exception } from './providers/exception';
 export { default as CrudController } from './crud/crud.controller';
